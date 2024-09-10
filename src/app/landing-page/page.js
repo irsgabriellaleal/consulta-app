e6602a21451681a11df49f33ecd7ad7154fa83ea
Iris Gabriella <iris.costa@oaz.co>
2024-09-09 21:30:42 -0300

    parte do banco

--- src/app/landing-page/page.js
+++ src/app/landing-page/page.js
@@ -85,7 +85,7 @@ export default function LandingPage() {
                 </div>
                 <div className="flex flex-col gap-2 min-[400px]:flex-row">
                   <Link
-                    href="#"
+                    href=""
                     className="inline-flex h-10 items-center justify-center rounded-md bg-gray-800 px-8 text-sm font-medium text-white shadow transition-colors hover:bg-black focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50"
                     prefetch={false}
                   >
